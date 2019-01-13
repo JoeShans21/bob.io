65babeb2c8e67ea6eb691d1bd287c58e0239a8ec
Joe Shanahan <shans.joe@gmail.com>
2019-01-13 17:38:21 -0500

    hggh

--- public/food.js
+++ public/food.js
@@ -2,13 +2,11 @@ function Food(x, y, img) {
   this.x = x;
   this.y = y;
   this.eaten = function() {
-    blobs.forEach(function (enemy) {
-      var d = createVector(x, y).dist(createVector(enemy.x, enemy.y));
-      if (d < 40 + enemy.r) {
-        socket.emit("removeme", x);
-        blob.r+=.1;
-      }
-    });
+    var d = createVector(x, y).dist(createVector(blob.x, blob.y));
+    if (d < 40 + enemy.r) {
+      socket.emit("removeme", x);
+      blob.r+=.1;
+    }
   }
   this.show = function() {
     image(brushes[img], this.x, this.y, 40, 40);
